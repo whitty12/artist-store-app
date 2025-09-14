f16ed59dac1d036c8b1c7643e13de36e5f14eecc
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-09-14 16:16:06 -0600

    Added new paths for a user and their cart, update functions still need to be fleshed out

--- app/controllers/promotion_controller.rb
+++
@@ -1,9 +0,0 @@
-class PromotionController < ApplicationController
-  def index
-    @promotions = Promotion.all
-  end
-
-  def show
-    @promotion = Promotion.find(params[:id])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,31 +4,21 @@ Rails.application.routes.draw do
   
   # Define our default resourceful routes
   resources :users do
-    resource :cart_items
+    resource :cart do
+      resources :cart_items, only: [:create, :destroy]
+    end
   end
   resources :products do
     resource :product_details
   end
   resources :promotions
   resources :creators
+  resources :product_types
 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "/user", to: "user#index"
-  get "/user/:id", to: "user#show" 
-  get "/product", to: "product#index"
-  get "/product/:id/product_details", to: "product_details#show"
-
-  get "/creator", to: "creator#index"
-  get "/creator/:id", to: "creator#show"
-  get "user/:id/cart", to: "cart#index"
-  get "/promotion", to: "promotion#index"
-  get "/promotion/:id", to: "promotion#show"
-  get "/product_type", to: "product_type#index"
-
-
+  get "up" => "rails/health#show", as: :rails_health_check  
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
