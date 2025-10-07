2602bc1f5bfb08c713580221ad51573ce81d8b17
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-10-07 11:43:13 -0600

    Product now adds cart items to the cart, user can delete the cart items from the cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
     belongs_to :promotion, optional: true
+    belongs_to :creator
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,22 @@ Rails.application.routes.draw do
   resources :promotions
   resources :creators
 
+  namespace :api do
+    namespace :v1 do
+      get '/users', to: 'users#index'
+      get '/users/:id', to: 'users#show'
+      get '/promotions', to: 'promotions#index'
+      get '/promotions/:id', to: 'promotions#show'
+      get '/products', to: 'products#index'
+      get '/products/:id', to: 'products#show'
+      get '/creators', to: 'creators#index'
+      get '/creators/:id', to: 'creators#show'
+      get '/cart', to: 'cart#show'
+    end
+  end
+
+
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check  
